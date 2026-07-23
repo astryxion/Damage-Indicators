a60fc866ea83b005a537ec2574dc508c018dccfc
Astryxion <s.pergolotti@my.ccsu.edu>
2026-07-23 06:00:58 -0400

    overhauled the mod

--- src/main/java/com/astryxion/damageindicators/MobTypes.java
+++ src/main/java/com/astryxion/damageindicators/MobTypes.java
@@ -2,15 +2,18 @@ package com.astryxion.damageindicators;
 
 import net.minecraft.resources.Identifier;
 import net.minecraft.tags.EntityTypeTags;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ambient.AmbientCreature;
 import net.minecraft.world.entity.animal.golem.AbstractGolem;
 import net.minecraft.world.entity.animal.fish.WaterAnimal;
 import net.minecraft.world.entity.monster.Enemy;
+import net.minecraft.world.entity.monster.Witch;
 import net.minecraft.world.entity.npc.Npc;
+import net.minecraft.world.entity.npc.villager.Villager;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.common.Tags;
-
 
 import java.util.Locale;
 
@@ -33,44 +36,75 @@ public enum MobTypes {
     BOSS;
 
     private final Identifier texture;
-    MobTypes(){
+
+    MobTypes() {
         texture = Identifier.fromNamespaceAndPath("damageindicators", "textures/gui/mob_types/" + name().toLowerCase(Locale.ROOT) + ".png");
     }
 
-    public static MobTypes getTypeFor(Entity entity){
-        if(entity instanceof Player){
+    public static MobTypes getTypeFor(Entity entity) {
+        if (entity instanceof Player) {
             return PLAYER;
         }
-        if(entity instanceof LivingEntity living){
-            if(living.getType().builtInRegistryHolder().is(Tags.EntityTypes.BOSSES)){
+        if (entity instanceof LivingEntity living) {
+            if (living.getType().builtInRegistryHolder().is(Tags.EntityTypes.BOSSES)) {
                 return BOSS;
             }
-            if(living.getType().builtInRegistryHolder().is(EntityTypeTags.AQUATIC)){
+            if (living.getType().builtInRegistryHolder().is(EntityTypeTags.AQUATIC) || living instanceof WaterAnimal || living.canBreatheUnderwater()) {
+                if (living.getType().builtInRegistryHolder().is(EntityTypeTags.ARTHROPOD)) {
+                    return WATER_ARTHROPOD;
+                }
                 return living instanceof Enemy ? WATER_MONSTER : WATER_ANIMAL;
-            }else if(living.getType().builtInRegistryHolder().is(EntityTypeTags.UNDEAD)){
+            }
+            if (living.getType().builtInRegistryHolder().is(EntityTypeTags.UNDEAD)) {
                 return living instanceof Enemy ? UNDEAD : UNDEAD_ANIMAL;
-            }else if(living.getType().builtInRegistryHolder().is(EntityTypeTags.ARTHROPOD)){
-                return living instanceof WaterAnimal || living.canBreatheUnderwater() ? WATER_ARTHROPOD : living instanceof Enemy ? ARTHROPOD_MONSTER : ARTHROPOD;
-            }else if(living.getType().builtInRegistryHolder().is(EntityTypeTags.ILLAGER)){
+            }
+            if (living.getType().builtInRegistryHolder().is(EntityTypeTags.ARTHROPOD)) {
+                return living instanceof Enemy ? ARTHROPOD_MONSTER : ARTHROPOD;
+            }
+            if (living.getType().builtInRegistryHolder().is(EntityTypeTags.ILLAGER) || living instanceof Witch) {
                 return ILLAGER;
             }
-            if(living instanceof AbstractGolem){
+            if (living instanceof AbstractGolem) {
                 return GOLEM;
             }
-            if(living instanceof Npc){
+            if (living instanceof Npc) {
                 return VILLAGER;
             }
-            if(living instanceof Enemy){
+            if (living instanceof Enemy) {
                 return MONSTER;
             }
-            if(living instanceof AmbientCreature){
+            if (living instanceof AmbientCreature) {
                 return AMBIENT;
             }
-            if(living instanceof Mob){
+            if (living instanceof Mob) {
                 return ANIMAL;
             }
         }
         return UNKNOWN;
+    }
+
+    /**
+     * Icon column index into the original DITypeIcons.png atlas (5 columns), matching Damage Indicators 1.12.2.
+     * 0 = undead/fire-immune, 1 = default living, 2 = humanoid, 3 = arthropod, 4 = boss.
+     */
+    public static int getCleanSkinIconIndex(LivingEntity entity) {
+        if (entity.getType().builtInRegistryHolder().is(Tags.EntityTypes.BOSSES)) {
+            return 4;
+        }
+        if (entity.getType().builtInRegistryHolder().is(EntityTypeTags.UNDEAD) || entity.fireImmune()) {
+            return 0;
+        }
+        if (entity.getType().builtInRegistryHolder().is(EntityTypeTags.ARTHROPOD)) {
+            return 3;
+        }
+        if (entity instanceof Player || entity instanceof Witch || entity instanceof Villager || entity instanceof AbstractGolem || entity instanceof Npc) {
+            return 2;
+        }
+        return 1;
+    }
+
+    public static boolean isHostileForCleanSkin(LivingEntity entity) {
+        return entity instanceof Enemy || entity.getType().builtInRegistryHolder().is(Tags.EntityTypes.BOSSES);
     }
 
     public Identifier getTexture() {
